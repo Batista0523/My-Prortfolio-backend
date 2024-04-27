9c62633d0af808ac7200ca1f47e1eec84d1b2961
elisaul batista <elisaulbatista@pursuit.org>
2024-04-26 22:27:28 -0400

    project description updated

--- db/seed.sql
+++ db/seed.sql
@@ -7,17 +7,26 @@ INSERT INTO works_portfolio (
     )
 VALUES (
         'CourtIQ',
-        'Explore CourtIQ: Your NBA game-changer, meticulously crafted by a collaborative team. Powered by React, Firebase, Express, and JavaScript, our platform seamlessly integrates data from approximately 10 APIs. From player stats to game odds, CourtIQ blends passion with precision, leveraging styled components for a sleek user experience. Elevate your NBA journey today.',
+        '🏀 Exciting News for NBA Fans! 🏀
+Introducing CourtIQ, the ultimate NBA fan experience! 🚀
+CourtIQ is a full-stack app meticulously crafted to elevate your NBA fandom and enhance your betting decisions. Developed in collaboration with my talented teammates Joram Mercado and Erick Tolentino, we’re proud of what we’ve accomplished together. 🤝
+A special thanks to our coaches and mentors for their invaluable guidance and support throughout the development process. Your expertise shaped CourtIQ into the app it is today. 🙏
+Key Technologies:
+	•	Frontend: React, SASS, CSS
+	•	Backend: Express.js, PostgreSQL
+	•	Additional Services: Firebase for real-time updates
+	•	APIs: Integrated with APIs for teams, players info, and bookmakers like DraftKings or FanDuel
+	•	Custom GPT: Our custom GPT answers user questions, and we’re continuously training it to provide an even better experience.
+Explore CourtIQ today and elevate your NBA journey! 🏀🔥',
         'https://github.com/Ericktolentino94/Capstone-front-end',
         'https://neon-dragon-3dc023.netlify.app/'
     );
-
 INSERT INTO contact_me(
         username,
         email,
         msg
     )
-    VALUES(
+VALUES(
         'Elisaul',
         'batista_pro1@hotmail.com',
         'hi i want to hire you'
